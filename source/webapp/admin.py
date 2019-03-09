9e1cf5ebb473dd2166c9607af7cbc65bc5b9a0fc
bak0903 <bak0903@mail.ru>
2019-03-09 11:55:28 +0600

    Сделал первый этап: cоздал модель Task, провел миграцию и подключил её к admin

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from webapp.models import Task
 
+
+class TaskAdmin(admin.ModelAdmin):
+    ordering = ['due_date']
+    search_fields = ['summary', 'description']
+
+
+admin.site.register(Task, TaskAdmin)
 
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,1 +1,22 @@
 from django.db import models
+
+
+class Task(models.Model):
+    STATUS_NEW = 'очередь'
+    STATUS_BOUGHT_OUT = 'в работе'
+    STATUS_CANCELED = 'сделано'
+
+    STATUS_CHOICES = (
+        (STATUS_NEW, 'очередь'),
+        (STATUS_BOUGHT_OUT, 'в работе'),
+        (STATUS_CANCELED, 'сделано')
+    )
+
+    summary = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    due_date = models.DateTimeField()
+    status = models.CharField(max_length=255, default=STATUS_NEW, choices=STATUS_CHOICES)
+    time_planned = models.DecimalField(max_digits=10, decimal_places=1, null=True, blank=True)
+
+    def __str__(self):
+        return '%s. Выполнить до %s' % (self.summary, self.due_date)
