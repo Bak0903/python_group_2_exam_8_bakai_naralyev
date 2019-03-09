081a40b82455882cfbb3264d2930136e1a560075
bak0903 <bak0903@mail.ru>
2019-03-09 11:56:47 +0600

    Сделал второй этап: добавил serializer, настроил view, urls - заработал api.

--- source/api_v/views.py
+++ source/api_v/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from webapp.models import Task
+from api_v.serializers import TaskSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all().order_by('due_date')
+    serializer_class = TaskSerializer
